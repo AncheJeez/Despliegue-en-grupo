017b643a417b0b6dfadb59a51ec5b745f31d576b
Juanchoo24 <github.goatskin743@passinbox.com>
2025-12-10 17:47:26 +0100

    subiendo cambios del config.php

--- app/Config.php
+++ app/Config.php
@@ -2,10 +2,10 @@
 
  class Config
  {
-     static public $mvc_bd_hostname = "mysql-los-hackers-alimentos.alwaysdata.net";
-     static public $mvc_bd_nombre   = "los-hackers-alimentos_2025";
-     static public $mvc_bd_usuario  = "443048";
-     static public $mvc_bd_clave    = "alimentos@";
+     static public $mvc_bd_hostname = "localhost";
+     static public $mvc_bd_nombre   = "alimentos52025";
+     static public $mvc_bd_usuario  = "Andres2025";
+     static public $mvc_bd_clave    = "Alimentos2025";
      static public $mvc_vis_css     = "estilo.css";
  }
 ?>
